356f43212b844a770aa1963289d5a632de2add02
Narasimha1997 <yogaimpression@gmail.com>
2019-02-05 20:20:26 +0530

    Added test to direct integer print

--- kernel/main.c
+++ kernel/main.c
@@ -26,17 +26,10 @@ void sample_program() {
     write("\t|___________|\n");
 
     //test conversion functions : 
-    char *string = "Narasimha";
+    int a = 30;
+    int b = 13;
 
-    uint16_t parity = generate_parity_32(string);
-
-    if(verify_parity_32("Narasimha", parity) == 1) fprint(STDOUT, "Parity matched\n");
-    else fprint(STDOUT, "Parity did not match here also!\n");
-
-    string = "Narasmmha";
-
-    if(verify_parity_32(string, parity) == 1) fprint(STDOUT, "Parity matched again\n");
-    else fprint(STDOUT, "Parity did not match this time\n"); 
+    print_int(a + b);
 
 }
 
